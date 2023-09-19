14105dd71badf47d1f28b77a3d01099cc1f30f13
Rahul P S <rahulps995@gmail.com>
2023-09-19 12:13:56 +0530

    readme update

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="w-screen h-[50px] bg-gray-700 text-white ">
+      <h1 className=" pt-2 text-2xl">Image Slider with Parallax Effect</h1>
+    </div>
+  );
+};
+
+export default NavBar;
